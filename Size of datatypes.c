c9dc6db66283120762fdd568002dfb364eea26f9
SahanaShreeVS <166837732+SahanaShreeVS@users.noreply.github.com>
2024-04-12 20:14:36 +0530

    Add files via upload

---
+++ Size of datatypes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     printf("Size of datatypes\n");
+     printf("Size of character :%ld\n",sizeof(char));
+     printf("Size of integer :%ld\n",sizeof(int));
+     printf("Size of float :%ld\n",sizeof(float));
+     printf("Size of double :%ld\n",sizeof(double));
+     getch();
+     }
